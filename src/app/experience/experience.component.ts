548031823ccc2aec1c9123656520482a177944df
Seung Lee <jkobb510@yahoo.com>
2023-05-29 20:37:48 -0400

    new files

---
+++ src/app/experience/experience.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.css']
+})
+export class HeaderComponent {
+
+}
